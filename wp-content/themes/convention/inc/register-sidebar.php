bab4609c7a1edfc3d9a26812f97f0e44b2522c1d
Nima Izadi <nim.izadi@gmail.com>
2013-05-15 11:57:18 +0200

    Updated style

--- wp-content/themes/convention/inc/register-sidebar.php
+++ wp-content/themes/convention/inc/register-sidebar.php
@@ -4,7 +4,7 @@ register_sidebar(array(
 'name' => __('Sidebar', 'convention'),
 'before_widget' => '',
 'after_widget' => '',
-'before_title' => '<div id="sidebar-widget"><div class="sidebar-heading">',
+'before_title' => '<div class="sidebar-widget"><div class="sidebar-heading">',
 'after_title' => '</div></p>',
 ));
 
